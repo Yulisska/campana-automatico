5ab4f1a8514ccd5de4bac6443091f4539d90d2d4
Yulia <maxymiv.yulia@gmail.com>
2023-02-26 16:16:12 +0200

    added: todos

--- main.py
+++ main.py
@@ -102,3 +102,6 @@ if True:
         time.sleep(timeout)
 
 print("it is over here...")
+# to do:
+# 1. read time table from https://docs.google.com/spreadsheets/d/1LX25qDzaKKtRPmRFZ9h7ZoTu1UjYz7yCt85Wwz13dbk/edit#gid=0
+# 2. control LED according to the time table 
